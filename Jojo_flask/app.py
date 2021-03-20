c95e9f84f9c43c86a0f1842fb1d2ff1f6f4d06b1
americam9 <amendoza22005@gmail.com>
2021-03-20 12:08:51 -0400

    updates/revisions using code shown in class

--- Jojo_flask/app.py
+++ Jojo_flask/app.py
@@ -1,25 +1,17 @@
 from flask import Flask, request, render_template, current_app as app
-from sense_hat import SenseHat
-from time import sleep
-
 app = Flask(__name__)
-sense = SenseHat()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == "POST":
-        thought = request.gorm.get("thought")
-        sense.show_thought(thought)
-   return render_template('index.html')
+   return render_template("login.html")
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/success',methods = ['GET', 'POST'])
 def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+   message = request.form['message']
+   name = request.form['name']
+   return render_template("success.html", message = message)
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
