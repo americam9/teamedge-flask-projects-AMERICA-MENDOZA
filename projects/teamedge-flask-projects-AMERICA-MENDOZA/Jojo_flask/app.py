3c06d9d5938437faee0ef12756709a33ab458beb
americam9 <amendoza22005@gmail.com>
2021-03-27 13:07:36 -0400

    changes on 3-27-21

--- projects/teamedge-flask-projects-AMERICA-MENDOZA/Jojo_flask/app.py
+++ projects/teamedge-flask-projects-AMERICA-MENDOZA/Jojo_flask/app.py
@@ -1,17 +1,23 @@
 from flask import Flask, request, render_template, current_app as app
+import requests
+from sense_hat import SenseHat
+from time import sleep
+
 app = Flask(__name__)
+sense = SenseHat()
 
 @app.route('/')
 def index():
-   return render_template("login.html")
+   return render_template("index.html")
 
 @app.route('/success',methods = ['GET', 'POST'])
 def success():
    message = request.form['message']
    name = request.form['name']
+   sense.show_message(message + " by " + name
    return render_template("success.html", message = message)
 
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = True, host='0.0.0.0')
