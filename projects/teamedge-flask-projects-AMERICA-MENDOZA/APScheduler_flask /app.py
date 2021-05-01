c1e9355b8ba987957bf0ac317aca7f97259ea6cb
americam9 <amendoza22005@gmail.com>
2021-05-01 14:05:02 -0400

    May 1st progress

--- projects/teamedge-flask-projects-AMERICA-MENDOZA/APScheduler_flask /app.py
+++ projects/teamedge-flask-projects-AMERICA-MENDOZA/APScheduler_flask /app.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 scheduler.init_app(app)
 scheduler.start()
 
-scheduler.add_job(id=unique_string_rowid, func=name name_of_function_you_want_to_run, 
-trigger= 'date', run_date= string_date_time_when_function_should_run, args = [any_arguments_
-you_want_to_pass_to_the_func]) 
-
-def show_reminder(reminder):
+@app.route('/send', methods= ['POST'])
+def inserttask():
+    task = request.form['task']
+    date = request.form['date']
+    due = request.form['due']
+    display = task + "sumbitted: " + date + "due by: " + due
+    sense.show_message(display)
+    return render_template('inserttask.html', task=task, date=date, due=due)
 
